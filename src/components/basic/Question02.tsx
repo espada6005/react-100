7eea229bdc1967d7b48f6607450162585d8cc709
espada <yua2479@gmail.com>
2026-04-26 21:04:51 +0900

    Question1～3

---
+++ src/components/basic/Question02.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  return (
+    <>
+      <h1>私のプロフィール</h1>
+      <p>Reactを学習中です</p>
+    </>
+  );
+};
+
+export default Profile;
+
---
+++ src/components/basic/Question03.tsx
@@ -0,0 +1,12 @@
+const Introduction = () => {
+  const name = '田中太郎';
+  const age = 25;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div>私は{name}です。{age}歳です。今年は{currentYear}年です。</div>
+  );
+
+};
+
+export default Introduction;
